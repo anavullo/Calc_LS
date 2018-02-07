a234e94f92a23cba4645eafad77b2e54048ef31b
Ana Vullo <aivullo@hotmail.com>
2018-02-07 16:59:12 +0100

    Llamando a la funcion para cargar mi raster especifico

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  7 16:29:04 2018
+import functions #Importo el fichero con las funciones
 
-@author: Usuario
-"""
+flow_acc = functions.cargar_raster('flowacc.tif') 
+#Llamo desde el fichero 'functions' a mi funcion 'cargar_raster', 
+#abro mi raster especifico, con el que quiero trabajar.
+
+#Una Otra forma de hacer lo anterior..
+
+import functions as fn
+
+flow_acc = fn.cargar_raster('flowacc.tif')
+
+
 
