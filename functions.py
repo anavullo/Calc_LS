b5d0e024275cd1a43949b272fffc3c50f07a1085
Ana Vullo <aivullo@hotmail.com>
2018-02-07 16:52:22 +0100

    Acomodando mejor la funcion

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import rasterio
 
 #Armamos la funcion para cargar cualquier raster
 def cargar_raster(nombre_raster): 
-raster = rasterio.open(nombre_raster) #Abrimos el fichero tif
-banda = raster.read(1) #Elegimos la banda con la que queremos trabajar
-
-return banda
+    raster = rasterio.open(nombre_raster) #Abrimos el fichero tif
+    banda = raster.read(1) #Elegimos la banda con la que queremos trabajar
+    
+    return banda
