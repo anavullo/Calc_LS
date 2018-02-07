c03f2ff03f4362e2adf7711b2d0d0f2f3c08898b
Ana Vullo <aivullo@hotmail.com>
2018-02-07 17:21:12 +0100

    Generando aspect para calculo b

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import functions #Importo el fichero con las funciones
+import gdal
 
 flow_acc = functions.cargar_raster('flowacc.tif') 
 #Llamo desde el fichero 'functions' a mi funcion 'cargar_raster', 
@@ -12,11 +13,20 @@ flow_acc = fn.cargar_raster('flowacc.tif')
 
 
 #Queremos multiplicar flow_acc por el area del pixel (lado), para calcular 
-# el area de captacion (a)
+# el area de contribucion aguas arriba (a)
 
 lado = 5
 
 a = flow_acc * lado**2 #Con dos * es la potencia
+
+##Calculo de la longitud efectiva de curva de nivel (b)
+
+#Tengo que generar una capa (aspect), para el calculo de b
+
+gdal.DEMProcessing('aspect.tf', 'fill_mde.tif', 'aspect') 
+#Nombre de archivo de salida, nombre archivo de entrada, calculo que quiero
+
+
 
 
 
