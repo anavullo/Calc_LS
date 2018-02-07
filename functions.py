3bf246f97efe2916e5e4b4af5fc3767444a9b0fc
Ana Vullo <aivullo@hotmail.com>
2018-02-07 16:51:00 +0100

    Creacion de una funcion mas general para cargar cualquier raster

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
 import rasterio
 
-flow_acc = rasterio.open('flowacc.tif')
+#Armamos la funcion para cargar cualquier raster
+def cargar_raster(nombre_raster): 
+raster = rasterio.open(nombre_raster) #Abrimos el fichero tif
+banda = raster.read(1) #Elegimos la banda con la que queremos trabajar
+
+return banda
